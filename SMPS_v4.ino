1725791b11fc48dfb0ff6bd2c5b2ac44a8bea9bc
James Fotherby <fotherja@gmail.com>
2016-11-09 20:12:00 +0000

    Add files via upload
    Improved the detection for whether or not a battery is connected.
    Better commenting etc
    This is now charges 12S LiPos to 50 volts

--- SMPS_v4.ino
+++ SMPS_v4.ino
@@ -1,15 +1,16 @@
 /* 
 Author: James Fotherby
   A program to control the output voltage of a non-synchronous boost converter used as a battery charger. A current sensor measures output current and
-  resistor dividors measure input and output voltages. The Duty cycle to the MOSFET is continuously adjusted to maintain the current output voltage and
+  resistor dividors measure input and output voltages. The Duty cycle to the MOSFET is continuously adjusted to maintain the set output voltage and
   current.
 
   To prevent control loop oscillations, the duty cycle is adjusted relatively slowly. This is fine for most applications but not all since the output voltage
   dips for a few douzen milliseconds on attachement of a load. The duty cycle is immediately cut back to zero if the output voltage exceeds a set threshold. This 
-  prevents damagingly high voltages from developing when a load suddenly is removed, which is a major issue with non-syncronous boost converters. 
+  prevents damagingly high voltages from developing when a load is suddenly removed, which is a major issue with non-syncronous boost converters. 
 
-  A test is done every 15 seconds or so to see whether disabling pwm causes the output voltage to drop to 19v. This would indicate nothing is connected to the output
-  and that it is RC decaying through the voltage dividor for the Arduino's ADC.
+  A test is done every 15 seconds to see whether disabling pwm causes the output voltage to drop towards 19v. This would indicate nothing is connected to the output
+  and that it is RC decaying through the voltage dividers for the Arduino's ADC. If such a scenario is detected the circuit idles and stops boosting. Reconnection 
+  of a battery starts things up again.
    
 */ 
 
@@ -27,8 +28,8 @@ Author: James Fotherby
                 
 // Variables
 int ICR_Reg = 299;                                                              // 53KHz PWM frequency                                                           
-float VoltageSetPoint = 32.8;                                                   // 4.125v per cell. Battery longevity increased by not fully charging it.
-float CurrentSetPoint = 1.4;                                                    // This value means under 4A input for the whole charge (inductor's saturation limit)
+float VoltageSetPoint = 50.0;       //###### VERY IMPORTANT #######             // Battery longevity increased by not fully charging it.
+float CurrentSetPoint = 0.8;        //###### VERY IMPORTANT #######             // This value means under 4A input for the whole charge (inductor's saturation limit) It slightly underestimates...
 float Vout;
 float Vin;
 float Iout;                                                             
@@ -39,7 +40,8 @@ unsigned long CurrentMicros[5] = {0,0,0,0,0};
 unsigned long PreviousMicros[5] = {0,0,0,0,0};
 unsigned long Interval[5] = {100, 5000, 60000, 500000, 15000000};
 
-float Charge_Levels[10] = {29.6, 30.4, 30.8, 31.2, 31.6, 32.0, 32.4, 32.8, 33.2, 33.8};
+//float Charge_Levels[10] = {29.6, 30.4, 30.8, 31.2, 31.6, 32.0, 32.4, 32.8, 33.2, 33.8};         // 8S
+float Charge_Levels[10] = {44.4, 45.6, 46.2, 46.8, 47.4, 48.0, 48.6, 49.2, 49.8, 50.7};           // 12S
 
 // Methods
 void Check_Battery_Status(void);
@@ -115,7 +117,7 @@ void loop()
 //##############################################################################
 void Over_Volt_Check()
 {
-  Vout = analogRead(VoutSense) * VOLTAGE_SENSE_CONST;                           // Convert Analogue read to real voltage value
+  Vout = (float)analogRead(VoutSense) * VOLTAGE_SENSE_CONST;                    // Convert Analogue read to real voltage value
   
   if(Vout > VoltageSetPoint + 2.0)                                              // If voltage goes 2 volts higher than the set point. Shut off immediately
   {
@@ -128,8 +130,8 @@ void Over_Volt_Check()
 //==============================================================================
 void Iterate_Control_Loop()
 {
-  Vout = analogRead(VoutSense) * VOLTAGE_SENSE_CONST;                           // Convert Analogue read to real voltage value
-  Iout = (analogRead(IoutSense)  - 512) * CURRENT_SENSE_CONST; 
+  Vout = (float)analogRead(VoutSense) * VOLTAGE_SENSE_CONST;                    // Convert Analogue read to real voltage value
+  Iout = ((float)analogRead(IoutSense)  - 512.0) * CURRENT_SENSE_CONST; 
   
   if((Vout < VoltageSetPoint) && (Iout < CurrentSetPoint))
   {                                                  
@@ -185,13 +187,13 @@ void Display_Debug_Data()
   Serial.println(Vin);  
   Serial.println();  
 
-  Vin = analogRead(VinSense) * VOLTAGE_SENSE_CONST;
+  Vin = (float)analogRead(VinSense) * VOLTAGE_SENSE_CONST;
   if(Vin < 17.0)
   {
     OCR1A = 0;
     Serial.println("Input voltage below 18 volts! PAUSED");
     
-    while((analogRead(VinSense) * VOLTAGE_SENSE_CONST) < 18.0) 
+    while(((float)analogRead(VinSense) * VOLTAGE_SENSE_CONST) < 18.0) 
     {
       LED_Duty = 5;
       Toggle_LED();
@@ -203,48 +205,66 @@ void Display_Debug_Data()
 //==============================================================================
 void Check_Battery_Status()
 {
-  OCR1A = 0;                                                                    // Switch off output
-  delay(100);                                                                   // And wait...
+  Serial.println("Battery Check");
+  
+  OCR1A = 0;                                                                    // Switch off output and immediately measure output voltage
+  delay(1);
 
-  float V_test = analogRead(VoutSense) * VOLTAGE_SENSE_CONST;
+  float Measured_Battery_Voltage = (float)analogRead(VoutSense) * VOLTAGE_SENSE_CONST;
+  Serial.print("Measured Battery Voltage: ");
+  Serial.println(Measured_Battery_Voltage);
 
-  // Checks if battery is fully charged. If so, the target voltage will be reached and current flow will be zero. Also checks for disconnect...
-  float TempStore = VoltageSetPoint;
+  static byte Suspect_Disconnected_Battery = 0;
+  float Voltage_to_Reach = VoltageSetPoint - 0.25;  
   byte counter = 0;
-  while(V_test >= TempStore && Iout <= 0.1)
+
+  // Checks if battery is fully charged. If so, the target voltage will be reached and current flow will be zero.
+  while(Measured_Battery_Voltage >= Voltage_to_Reach && Iout <= 0.1)            // If the standalone Battery Voltage >= The Set point and less than 0.1A is flowing stay in this loop 
   { 
-    TempStore = VoltageSetPoint - 0.25;
+    Voltage_to_Reach = VoltageSetPoint - 0.5;                                   // Add a bit of Hysterisis in. This makes it easier to stay in this loop by reducing the set point slightly once here
     
-    LED_Duty = 10;
+    LED_Duty = 10;                                                              // Simply ensures LED is always on
     Toggle_LED();
     delay(10);  
 
-    counter++;
+    counter++;                                                                  // Increment counter every 10ms whilst in this loop. 
     counter = min(counter, 100);
-    if(counter == 99)    {      
+    if(counter == 99)    {                                                      // If we're staying in this loop it means a battery must be connected. And it must be fully charged     
       Serial.println("Battery Fully Charged... PAUSED");
+      Suspect_Disconnected_Battery = 0;
     }
     
-    V_test = analogRead(VoutSense) * VOLTAGE_SENSE_CONST;
+    Measured_Battery_Voltage = (float)analogRead(VoutSense) * VOLTAGE_SENSE_CONST;
   }
 
-  // The decay was fast enough to suspect it was due to self discharge. Otherwise the battery would maintain the voltage.
-  static byte Suspect_Disconnected_Battery = 0;
-  if(counter > 0 && counter < 15)  {
-    Suspect_Disconnected_Battery++;
-  }  else  {
-    Suspect_Disconnected_Battery -= 2;
-    Suspect_Disconnected_Battery = max(Suspect_Disconnected_Battery, 0);    
+  Serial.print("counter: ");
+  Serial.println(counter);  
+  
+  if(counter == 0)                                                              // We haven't charged the battery yet! (Or has the Measured_Battery_Voltage been pulled down very quickly?)  
+  {  
+  }  
+  else if(counter > 1 && counter < 30)                                          // The decay was fast enough to suspect it was due to self discharge. Otherwise the battery would have maintained the voltage.
+  {
+    Suspect_Disconnected_Battery++;    
+  }  
+  else                                                                          // The decay was quite slow. Unlikely to be because of a disconnected output
+  {
+    Suspect_Disconnected_Battery = 0;   
   }
+
+  Serial.print("SDB count: ");
+  Serial.println(Suspect_Disconnected_Battery);   
 
   // If we've had 4 Suspect_Disconnected_Battery +ve tests do a final check then indicate such condition and wait until a battery is connected 
   if(Suspect_Disconnected_Battery >= 4)
   {
-    delay(7500);                                                                // Should allow enough time for voltage to decay to ~19volts
-    Suspect_Disconnected_Battery = 0;
-    V_test = analogRead(VoutSense) * VOLTAGE_SENSE_CONST;
+    Serial.println("Suspect Disconnected Battery!");
+    delay(15000);                                                               // Should allow enough time for voltage to decay to ~19volts
     
-    while(V_test < 22.0)
+    Suspect_Disconnected_Battery = 0;
+    Measured_Battery_Voltage = (float)analogRead(VoutSense) * VOLTAGE_SENSE_CONST;
+    
+    while(Measured_Battery_Voltage < 22.0)
     {
       LED_Duty = 0;
       Toggle_LED(); 
@@ -253,20 +273,20 @@ void Check_Battery_Status()
       LED_Duty = 10;
       Toggle_LED(); 
       delay(50);
-
-      Serial.println("Battery Disconnected... PAUSED");
-      V_test = analogRead(VoutSense) * VOLTAGE_SENSE_CONST;
+      
+      Measured_Battery_Voltage = (float)analogRead(VoutSense) * VOLTAGE_SENSE_CONST;
+      Serial.print("Battery Disconnected... PAUSED. Voltage on output: ");
+      Serial.println(Measured_Battery_Voltage);
     }
   }
-
 
   // Flash the LED at a duty that represents the batteries charge status.
   LED_Duty = 1;                                                           
   for(int i = 0; i < 9; i++)
   {
-    if(V_test > Charge_Levels[i])
+    if(Measured_Battery_Voltage > Charge_Levels[i])
       LED_Duty++;
-  }  
+  }    
 }
 
 
